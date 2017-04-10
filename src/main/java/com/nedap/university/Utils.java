71fa1557aa74773fc589d293e9ede1fef7d4903b
MinThamie <a.g.vanherwijnen@gmail.com>
2017-04-10 20:39:19 +0200

    Created a utils file with bytesToHex function and bytesToInt function

---
+++ src/main/java/com/nedap/university/Utils.java
@@ -0,0 +1,26 @@
+package com.nedap.university;
+
+import java.util.Formatter;
+
+/**
+ * Created by anne-greeth.vanherwijnen on 10/04/2017.
+ */
+public class Utils {
+    public static String bytesToHex(byte[] bytes){
+        Formatter formatter = new Formatter();
+        for (byte b : bytes) {
+            formatter.format("%02x", b); //TODO: find out how this works
+        }
+        return formatter.toString();
+    }
+
+
+    public static int bytesToInt(byte[] bytes){
+        long value = 0;
+        for (int i = 0; i < bytes.length; i++)
+        {
+            value = (value << 8) + (bytes[i] & 0xff); //TODO: Find out how this works
+        }
+        return (int) value;
+    }
+}
