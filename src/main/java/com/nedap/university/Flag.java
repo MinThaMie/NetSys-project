42422fd98e1aeec1f2b21d5b900706a48d8ce1de
Anne-Greeth van Herwijnen <anne-greeth.vanherwijnen@nvc1710.nedap.local>
2017-04-11 09:04:00 +0200

    Added flag enum

---
+++ src/main/java/com/nedap/university/Flag.java
@@ -0,0 +1,29 @@
+package com.nedap.university;
+
+/**
+ * Created by anne-greeth.vanherwijnen on 11/04/2017.
+ */
+public enum Flag {
+    SYN(1), ACK(2), FIN(4), FILES(8), PAUSE(16), DNS(32);
+    int value;
+
+    Flag(int value){
+        this.value = value;
+    }
+
+    public int setFlags(Flag[] flags){
+        int flagsvalue = 0;
+        for(Flag f : flags){
+            flagsvalue =+ f.value;
+        }
+        return flagsvalue;
+    }
+
+    public void receivedFlags(int receivedFlagValue){ //TODO: think of a nice way to do this?
+
+    }
+
+    public static boolean isSet(Flag flag, int receivedFlagValue){
+        return (receivedFlagValue & flag.value) > 0;
+    }
+}
