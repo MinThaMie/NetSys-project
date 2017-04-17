3bddb8ad32eda8916cc496119ace65d7ab962c3a
Anne-Greeth van Herwijnen <anne-greeth.vanherwijnen@nvc1710.nedap.local>
2017-04-17 12:09:32 +0200

    Added plaatje2 to files and removed it from the root folder + Added fileListTest

---
+++ src/test/java/com/nedap/university/fileListTest.java
@@ -0,0 +1,34 @@
+package com.nedap.university;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by anne-greeth.vanherwijnen on 16/04/2017.
+ */
+public class fileListTest {
+    private String filePath = "files";
+    private File file;
+    private String[] files;
+    @Before
+    public void SetUp(){
+        file = new File(filePath);
+        files = file.list();
+    }
+
+    @Test
+    public void fileListTest(){
+        assertThat(files.length, is(3));
+    }
+
+    @Test
+    public void arrayToString(){
+        String fileString = Utils.stringArrayToString(files);
+        TerminalOutput.showFiles(fileString);
+    }
+}
