871613c7de2ea6d65dc2e3acdf4115d9b4357a23
Anne-Greeth van Herwijnen <anne-greeth.vanherwijnen@nvc1710.nedap.local>
2017-04-18 23:20:53 +0200

    Remove typo from Static.HEADERLENGTH

--- src/test/java/com/nedap/university/utils/filePrepTest.java
+++ src/test/java/com/nedap/university/utils/filePrepTest.java
@@ -28,50 +28,50 @@ public class filePrepTest {
     private File originalFilePNG;
     private File reassembeldFilePNG;
 //TODO: uncomment
-//    @Before
-//    public void SetUp(){
-//        originalFileJPG = new File("files/photo1.jpg");
-//        myData = FilePrep.getBytesFromFile(originalFileJPG);
-//        LinkedList<byte[]> choppedData = FilePrep.filePrep(originalFileJPG);
-//        reassembledData = FilePrep.getByteArrayFromByteChunks(choppedData);
-//        Utils.setFileContentsClient(reassembledData, 3, "jpg"); //This is commented out because it otherwise creates a picture every build
-//        reassembeldFileJPG = new File("files/plaatje3.jpg");
-//
-//        //PNG test
-//        originalFilePNG = new File("files/rdtcInput3.png");
-//        myDataPNG = FilePrep.getBytesFromFile(originalFilePNG);
-//        LinkedList<byte[]> choppedDataPNG = FilePrep.getBytesToPacketSize(myDataPNG);
-//        reassembledDataPNG = FilePrep.getByteArrayFromByteChunks(choppedDataPNG);
-//        Utils.setFileContentsClient(reassembledDataPNG, 3, "png"); //This is commented out because it otherwise creates a picture every build
-//        reassembeldFilePNG = new File("files/plaatje3.png");
-//    }
-//
-//
-//    @Test
-//    public void testAmountOf() {
-//        assertThat(FilePrep.amountOfPacketsNeeded(testArray1974), is(2));
-//        assertThat(FilePrep.amountOfPacketsNeeded(testArray2013), is(2));
-//        assertThat(FilePrep.amountOfPacketsNeeded(testArray4000), is(3));
-//    }
-//
-//    @Test
-//    public void testgetPartialArrays() {
-//        assertThat(FilePrep.getBytesToPacketSize(testArray1974).size(), is(2));
-//        assertThat(FilePrep.getBytesToPacketSize(testArray2013).size(), is(2));
-//        assertThat(FilePrep.getBytesToPacketSize(testArray4000).size(), is(3));
-//    }
-//
-//    @Test
-//    public void testChopping() { //Assumption is that the same byteArray results in the same picture :)
-//        assertArrayEquals(reassembledData,myData);
-//    }
-//
-//    @Test
-//    public void testCheckSumJPG() throws Exception{
-//        assertArrayEquals(Utils.createSha1(originalFileJPG), Utils.createSha1(reassembeldFileJPG));
-//    }
-//    @Test
-//    public void testCheckSumPNG() throws Exception{
-//        assertArrayEquals(Utils.createSha1(originalFilePNG), Utils.createSha1(reassembeldFilePNG));
-//    }
+    @Before
+    public void SetUp(){
+        originalFileJPG = new File("files/photo1.jpg");
+        myData = FilePrep.getBytesFromFile(originalFileJPG);
+        LinkedList<byte[]> choppedData = FilePrep.filePrep(originalFileJPG);
+        reassembledData = FilePrep.getByteArrayFromByteChunks(choppedData);
+        Utils.setFileContentsClient(reassembledData, 3, "jpg"); //This is commented out because it otherwise creates a picture every build
+        reassembeldFileJPG = new File("files/plaatje3.jpg");
+
+        //PNG test
+        originalFilePNG = new File("files/rdtcInput3.png");
+        myDataPNG = FilePrep.getBytesFromFile(originalFilePNG);
+        LinkedList<byte[]> choppedDataPNG = FilePrep.getBytesToPacketSize(myDataPNG);
+        reassembledDataPNG = FilePrep.getByteArrayFromByteChunks(choppedDataPNG);
+        Utils.setFileContentsClient(reassembledDataPNG, 3, "png"); //This is commented out because it otherwise creates a picture every build
+        reassembeldFilePNG = new File("files/plaatje3.png");
+    }
+
+
+    @Test
+    public void testAmountOf() {
+        assertThat(FilePrep.amountOfPacketsNeeded(testArray1974), is(2));
+        assertThat(FilePrep.amountOfPacketsNeeded(testArray2013), is(2));
+        assertThat(FilePrep.amountOfPacketsNeeded(testArray4000), is(3));
+    }
+
+    @Test
+    public void testgetPartialArrays() {
+        assertThat(FilePrep.getBytesToPacketSize(testArray1974).size(), is(2));
+        assertThat(FilePrep.getBytesToPacketSize(testArray2013).size(), is(2));
+        assertThat(FilePrep.getBytesToPacketSize(testArray4000).size(), is(3));
+    }
+
+    @Test
+    public void testChopping() { //Assumption is that the same byteArray results in the same picture :)
+        assertArrayEquals(reassembledData,myData);
+    }
+
+    @Test
+    public void testCheckSumJPG() throws Exception{
+        assertArrayEquals(Utils.createSha1(originalFileJPG), Utils.createSha1(reassembeldFileJPG));
+    }
+    @Test
+    public void testCheckSumPNG() throws Exception{
+        assertArrayEquals(Utils.createSha1(originalFilePNG), Utils.createSha1(reassembeldFilePNG));
+    }
 }
